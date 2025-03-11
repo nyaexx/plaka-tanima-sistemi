f7028553d5995ec2dde7fa3c564deb1973e5e344
Hamzahsl <hamza.huseyinogluu41@gmail.com>
2025-03-11 11:18:52 +0300

    Update alg2_model_m_o.py
    GG Ez

--- Plaka-Okuma/Plaka Tanıma/alg2_model_m_o.py
+++ Plaka-Okuma/Plaka Tanıma/alg2_model_m_o.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 import pandas as pd
+
 import pickle
+
 import tensorflow as tf
 
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier # kutupahemiz
 
 import os
 
@@ -15,7 +17,6 @@ tek_batch = 0
 
 urls = []
 sinifs = []
-
 for sinif in siniflar:
     resimler = os.listdir(path+sinif)
     for resim in resimler:
@@ -23,10 +24,13 @@ for sinif in siniflar:
         sinifs.append(sinif)
         tek_batch+=1
 
+    
 df = pd.DataFrame({"adres":urls,"sinif":sinifs})
 
+
 def islem(img):
-    yeni_boy = img.reshape((166,5,5))
+    
+    yeni_boy = img.reshape((1600,5,5))
     orts = []
     for parca in yeni_boy:
         ort = np.mean(parca)
@@ -38,5 +42,36 @@ def islem(img):
 def on_isle(img):
     return img/255
 
-target_size = (200,200)
-batch_size = tek_batch
+target_size=(200,200) 
+batch_size=tek_batch 
+
+
+train_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+    preprocessing_function=on_isle)
+
+train_set = train_gen.flow_from_dataframe(df,x_col = "adres",y_col="sinif",
+                                              target_size=target_size,
+                                              color_mode = "grayscale",
+                                              shuffle=True,
+                                              class_mode='sparse',
+                                              batch_size=batch_size)
+
+
+images,train_y = next(train_set)
+train_x = np.array(list(map(islem,images))).astype("float32")
+train_y= train_y.astype(int)
+
+
+print("random forest / Rassal orman şeklinde egitiliyor")
+
+rfc = RandomForestClassifier(n_estimators=10,criterion="entropy")
+
+rfc.fit(train_x,train_y)
+
+print("Egitildi")
+
+pred = rfc.predict(train_x)
+
+acc = accuracy_score(pred,train_y)
+
+print("Başarili")
